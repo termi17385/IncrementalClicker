aea0fd263317bba5605b164a274b7ac7c2155874
termi17385 <66066987+termi17385@users.noreply.github.com>
2020-10-29 20:45:57 +1100

    removing random crap

--- Assets/Scripts/IncrementalClicker/EnhancementButtons/Enhancement.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-// For Upgrade 1, 2, 3, 4
-[Serializable]
-public class Enhancement : Upgrade
-{
-    public int maxUse = 5;
-    public int counter = 0;
-    public float moneyFactor = 1f;
-}
